fbebd18861d25076d06364b91c96328f5b48a2a8
Ina Vasilevskaya <inavasilevskaya@gmail.com>
2016-02-28 04:26:21 +0300

    static: some client refactor

--- src/static/js/chat.js
+++ src/static/js/chat.js
@@ -1,28 +1,48 @@
 var state = "chat";
+var client = "";
 $(function() {
 
 var conn;
 var msg = $("#msg");
 var log = $("#log");
 
-function appendLog(msg, side) {
-    var opp_side = "left";
-    if (side == "left") {
-        var opp_side = "right"
-    }
+function appendLog(html) {
     var d = log[0]
     var doScroll = d.scrollTop == d.scrollHeight - d.clientHeight;
-    log.append("<li class='clearfix " + side + "'>" +"<div class='chat-body clearfix'><div class='header'><strong class='pull-" +
-               opp_side + " primary-font'>Bhaumik Patel</strong></div><p>" + msg.text() + "</p></div></li>")
+    log.append(html)
     /*if (doScroll) {
         d.scrollTop = d.scrollHeight - d.clientHeight;
     }*/
 }
 
+function getColor() {
+    var range = "0123456789abcdef";
+    var color = "";
+    var i = 0;
+    while(i < 6) {
+        color += range[Math.floor(Math.random() * 16)];
+        i++;
+    }
+    return color;
+}
+
+function publishMessage(text, author) {
+    var side = "left";
+    var opp_side = "right";
+    if (client == author) {
+        side = "right";
+        opp_side = "left";
+    }
+    var html = "<li class='clearfix " + side + "'><span class='chat-img pull-'" + side + 
+        "><img src='http://placehold.it/50/" + getColor() + "/fff&text=U' class='img-circle' /></span>" +
+        "<div class='chat-body clearfix'><div class='header'><strong class='pull-" +
+        opp_side + " primary-font'>" + author + "</strong></div><p>" + text + "</p></div></li>"
+    return appendLog(html, side)
+}
+
 function changeState(newState) {
     if (newState == "game") {
         $("#pressBut").text("Answer");
-        msg.attr("disabled", true);
         $("#pressBut").focus()
     } else if (newState == "answer") {
         msg.attr("disabled", false);
@@ -66,15 +86,22 @@ if (window["WebSocket"]) {
         appendLog($("<div><b>Connection closed.</b></div>"), "left")
     }
     conn.onmessage = function(evt) {
-        appendLog($("<div/>").text(evt.data), "left")
+        data = JSON.parse(evt.data);
+        if (data.Type == "whoami") {
+            client = data.Name;
+            isMaster = data.IsMaster;
+            return
+        }
+        var text = data.Text
+        publishMessage(data.Text, data.Name);
             // FIXME XXX damn, that's awful, change data from server to json (state, msg)
-            if (evt.data.search("Game Mode") > -1) {
+            if (data.State == "game") {
                 changeState("game");
-            } else if (evt.data.search("Chat Mode") > -1) {
+            } else if (data.State == "chat") {
                 changeState("chat");
-            } else if (evt.data.search("your answer") > -1) {
+            } else if (text.search("your answer") > -1) {
                 changeState("answer");
-            } else if (evt.data.search("Game Reset") > -1 || evt.data.search("Time is Out") > -1) {
+            } else if (text.search("Game Reset") > -1 || text.search("Time is Out") > -1) {
                 changeState("game");
             }
     }
